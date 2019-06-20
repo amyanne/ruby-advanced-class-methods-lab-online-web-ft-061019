6b45cb6c3db875a5928418ee71cb751b8e77d028
Amy Westcott <amy.westcott@yahoo.com>
2019-06-20 20:48:01 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -40,9 +40,5 @@ class Song
    @@all.sort_by{|word| word.name}
   end
   def self.new_from_filename(name)
-    song = self.new
-    song.name = name.split(" - ")[1].chomp(".mp3")
-    song.artist_name = name.split(" - ")[0]
-    puts song
-  end
+    
 end
