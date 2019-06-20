5c3437639a71725bdbe0580e73d9cba8247eae28
Amy Westcott <amy.westcott@yahoo.com>
2019-06-20 20:51:39 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -45,7 +45,7 @@ class Song
     song.artist_name = name.split(" - ")[0]
     song
   end
-  def self.create_from_file
+  def self.create_from_filename
     song = self.new
     song.name = name.split(" - ")[1].chomp(".mp3")
     song.artist_name = name.split(" - ")[0]
