021124733b168792648feaebf2448b063ae9b5f0
Amy Westcott <amy.westcott@yahoo.com>
2019-06-20 20:44:43 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -39,7 +39,7 @@ class Song
   def self.alphabetical
    @@all.sort_by{|word| word.name}
   end
-  def self.new_from_filename(name, artist_name)
+  def self.new_from_filename(name)
     song = self.new
     song.name = name.split(" - ")[1].chomp(".mp3")
     song.artist_name = name.split(" - ")[0]
